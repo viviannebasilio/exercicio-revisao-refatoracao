a81b5ccf19b2055923368f42a8a4c0f3d14336e0
viviannebasilio <55855943+viviannebasilio@users.noreply.github.com>
2020-10-20 15:58:17 -0300

    Update Empregado.cpp

--- Empregado.cpp
+++ Empregado.cpp
@@ -1,1 +1,42 @@
+#include "Empregado.hpp"
 
+Empregado::Empregado(std::string _nome, double _salarioHora, double _quotaMensalVendas){
+    nome = _nome;
+    salarioHora = _salarioHora;
+    quotaMensalVendas = _quotaMensalVendas;
+}
+
+Empregado::Empregado(std::string _nome, double _salarioHora){
+      nome = _nome;
+      salarioHora = _salarioHora;
+}
+    
+double Empregado::pagamentoMes(double horasTrabalhadas){
+  double totalHoras = horasTrabalhadas;
+	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
+      if (horasTrabalhadas > HORAS_MAXIMO) {
+        double horasExcedido = horasTrabalhadas - HORAS_MAXIMO;
+        totalHoras += horasExcedido / 2;
+      }
+	  return totalHoras * salarioHora;
+}
+
+double Empregado::getsalarioHora(){
+    return salarioHora;
+}
+double Empregado::getquotaMensalVendas(){
+    return quotaMensalVendas;
+}
+std::string Empregado::getNome(){
+    return nome;
+}
+void Empregado::setNome(std::string Nome){
+    nome = Nome;
+}
+void Empregado::setsalarioHora(double Salario){
+    salarioHora = Salario;
+}
+void Empregado::setquotaMensalVendas(double Quota){ 
+    quotaMensalVendas = Quota;
+	
+};
