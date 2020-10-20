2687f78ebc458a1352532a59729ecd34f45a4c24
viviannebasilio <55855943+viviannebasilio@users.noreply.github.com>
2020-10-20 15:59:38 -0300

    Update Engenheiro.hpp

--- Engenheiro.hpp
+++ Engenheiro.hpp
@@ -1,1 +1,18 @@
+#ifndef ENGENHEIRO_H
+#define ENGENHEIRO_H
+#include "Empregado.hpp"
 
+class Engenheiro:public Empregado{
+
+  private:
+    int projetos;
+
+  public:
+    Engenheiro(std::string _nome, double _salarioHora, int _projetos);
+
+    int getProjetos();
+    void setProjetos(int _projetos);
+    void informacao(double horas);
+};
+
+#endif
